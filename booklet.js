08b19628a663cab57d099aa155af907a2212216e
agustin mateos <agustinrotondomateos@gmail.com>
2023-06-11 15:39:09 -0300

    book

---
+++ booklet.js
@@ -0,0 +1,87 @@
+// let nombre=prompt("asginar nombre")
+// let apellido=prompt("asignar apellido")
+// let edad=prompt("asignar edad")
+
+// const ciudad1="california"
+// const ciudad2="new york"
+// const ciudad3="buenas aires"
+// const ciuad4="rosario"
+// const ciudad5="la plata"
+
+// let codigoCarnet="dd23c4f"
+// let nombreCarnet="agustin mateos"
+// let direccioncarnet="french 2756"
+// let fechaNacimineto="04/11/95"
+// let sexo="masculino"
+
+// let carnet="codigo de carnet: "+codigoCarnet+"  "
+// +nombreCarnet+direccioncarnet+fechaNacimineto+sexo;
+
+// console.log(carnet)
+
+// let nombrePadre=prompt("ingresar nombre padre");
+// let nombreMadre=prompt("ingresar nombre madre");
+// let nombreHijo=prompt("ingresar nombre hijo");
+// let nombreHija=prompt("ingresar nombre hija");
+// let nombreMascota=prompt("ingresar nombre mascota");
+
+// let familia=nombrePadre+nombreMadre+nombreHija+nombreHijo+nombreMascota
+
+// alert(familia)
+
+// let montoProducto=prompt("ingresar precio producto")
+
+// let montoProducto20=(montoProducto * 20)/100
+// let montoProducto30=(montoProducto *30)/100
+
+// alert("el monto de tu producto es: "+montoProducto+""+"el precio con 20 de descuento quedaria en: "+montoProducto20)
+
+// let entrada=prompt("ingresar nombre")
+// let nombre="agustin"
+
+// if (nombre== entrada){
+//     alert ("soy agustin perras")
+// }else{alert("tu eres quien deberia ser")}
+
+
+// let letra=prompt("dime una letra")
+
+// if ((letra=="y")||(letra=="Y") ){
+// alert("correcto")
+// }else{alert("incorrecto")}
+
+// let ingresarNumero=prompt("ingresar numero del 1 al 4")
+
+//  if (ingresarNumero==1) {
+//     alert("eres bart")
+//  }else if(ingresarNumero==2){
+//     alert("eres flanders")
+//  }else if(ingresarNumero==3){
+//     alert("eres agustin")
+//  }else if(ingresarNumero==4){
+//     alert("eres carmen")
+//  }
+//  else{"eres lautaro"}
+
+
+// let ingresarNumero=prompt("ingresar presupuesto ")
+
+//  if ((ingresarNumero >0)&(ingresarNumero  <=1000)) {
+//     alert("presupuesto bajo")
+//  }else if((ingresarNumero >=1001)&(ingresarNumero<=3000)){
+//     alert("presupuesto medio")
+//  }else if(ingresarNumero>=3000){
+//     alert("presupuesto alto")}
+//  else{"eres lautaro"}
+
+
+// let ingresarProducto1=prompt("ingresar producto1")
+// let ingresarProducto2=prompt("ingresar producto2")
+// let ingresarProducto3=prompt("ingresar producto3")
+// let ingresarProducto4=prompt("ingresar producto4")
+// let ingresarProducto5=prompt("ingresar producto5")
+
+// if ((ingresarProducto1 != "")&&(ingresarProducto2!="")&&(ingresarProducto3!="")&&(ingresarProducto4!="")&&(ingresarProducto5)){
+//     let order=ingresarProducto1 + ingresarProducto2 + ingresarProducto3 + ingresarProducto4 + ingresarProducto5
+//     alert(order)
+// }else{alert("error en la carga de datos")}
--- clase2.js
+++ clase2.js
@@ -1,9 +1,31 @@
-let iNombre=prompt("ingresar nombre")
-let iApellido=prompt("ingresar apellido")
-let iEdad=prompt("ingresar edad")
+// let iNombre=prompt("ingresar nombre")
+// let iApellido=prompt("ingresar apellido")
+// let iEdad=prompt("ingresar edad")
 
-if ((iNombre != "")&&(iEdad > 18)){
-alert("bienvenido" +iNombre+ iApellido)
-console.log("sss")
-}
-else{alert(iNombre + "es menor de edad, no puede entrar")}
+// if ((iNombre != "")&&(iEdad > 18)){
+// alert("bienvenido" +iNombre+ iApellido)
+// console.log("sss")
+// }
+// else{alert(iNombre + "es menor de edad, no puede entrar")}
+
+// for (let i=1; i<=10;i++){if(i==5){break}alert(i)}
+ 
+
+// let entrada=prompt("ingresar nombre");
+
+// while (entrada != "ESC"){
+//     alert(entrada)
+//     entrada=prompt('ingresar otro dato')
+// }
+// let repetir = false;
+// do{
+// console.log("¡Solo una vez!");
+// }while(repetir)
+
+let numero = 2;
+do{
+   //Repetimos con do...while mientras el usuario ingresa un n°
+   numero = prompt("Ingresar Número");
+   console.log(numero);
+//Si el parseo no resulta un número se interrumpe el bucle.   
+}while(parseInt(numero));
