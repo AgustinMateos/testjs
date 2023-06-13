ca1343074fe0696163b17ffedac40655e2b8e2f7
agustin mateos <agustinrotondomateos@gmail.com>
2023-06-13 00:05:34 -0300

    book

--- booklet.js
+++ booklet.js
@@ -85,3 +85,312 @@
 //     let order=ingresarProducto1 + ingresarProducto2 + ingresarProducto3 + ingresarProducto4 + ingresarProducto5
 //     alert(order)
 // }else{alert("error en la carga de datos")}
+
+// for (let i=1; i<=10;i++){
+//     let texto=prompt("ingresar texto")
+//     let numero=prompt('ingresar numero')
+//     if (numero == 5){
+//         break
+//     }
+// }
+
+// let cantidad = prompt('INGRESAR CANTIDAD DE REPETICIONES');
+// let texto    = prompt('INGRESAR TEXTO A REPETIR');
+// for (let index = 0; index < cantidad; index++) {
+//     console.log(texto);
+// }
+
+
+// let lados = prompt('INGRESAR CANTIDAD DE LADOS');
+// for (let index = 0; index < lados; index++) {
+//     if (index > 3) {
+//         break;
+//     }
+//     alert("lado");
+// }
+
+
+// let alumnos = '';
+// for (let index = 0; index < 10; index++) {
+//     alumnos += prompt("INGRESAR NOMBRE DE ALUMNO")+"\n";
+// }
+// alert(alumnos);
+
+// let entrada    = prompt("INGRESAR NOMBRE");
+// let ingresados = '';
+// while (entrada != 'Voldemort') {
+//     ingresados += entrada +"\n";
+//     entrada     = prompt("INGRESAR NOMBRE");
+// }
+// // alert(ingresados);
+// let entrada =  prompt("SELECCIONAR PRODUCTO DE 1 A 4");
+// while (entrada != 'ESC') {
+//     switch (entrada) {
+//         case "1":
+//             alert("Tomate");
+//             break;
+//         case "2":
+//             alert("Papa");
+//             break;
+//         case "3":
+//             alert("Carne");
+//             break;
+//         case "4":
+//             alert("Pollo");
+//             break;
+//         default:
+//             alert("Error");
+//             break;
+//     }
+//     let entrada =  prompt("SELECCIONAR PRODUCTO DE 1 A 4");
+// while (entrada != 'ESC') {
+//     switch (entrada) {
+//         case "1":
+//             alert("Tomate");
+//             break;
+//         case "2":
+//             alert("Papa");
+//             break;
+//         case "3":
+//             alert("Carne");
+//             break;
+//         case "4":
+//             alert("Pollo");
+//             break;
+//         default:
+//             alert("Error");
+//             break;
+//     }
+//     entrada = prompt("SELECCIONAR PRODUCTO DE 1 A 4");
+// }
+
+// }
+  
+// function valor1() {
+//      return prompt("ingresar valor")
+// }
+//  function procesamiento(valor){
+//  return "el valor es:" + valor}
+
+//  function salida(valor){alert (valor)}
+ 
+
+//  salida(procesamiento(valor1()))
+
+
+// Codificar una función que reciba un número decimal por parámetro, y lo retorne redondeado al entero más cercano. Para ello, puede emplearse  Math.round.
+// Luego invocar la función de forma iterativa cinco (5) veces, solicitando en cada ciclo un número al usuario.
+
+
+// function redondeo(valor){
+//     return Math.round(valor)//creamos la funcion de redondeo
+// }
+// for (let index = 0; index < 5; index++) {
+//     let entrada = prompt("INGRESAR NUMERO"); //se crea el bucle que repetira la solicitud las veces indicadas y devuelve el valor redondado
+//     alert(redondeo(entrada));
+// }
+
+// Codificar una función con la siguiente cabecera: impuesto(precio, porcentaje). En ella, se retorna un valor numérico compuesto por el precio recibido, más la suma del porcentaje ingresado.
+// Luego invocar la función de forma iterativa cinco (5) veces, solicitando en cada ciclo un precio y porcentaje al usuario.
+
+// function impuesto(precio, porcentaje){
+//     return precio + ((precio * porcentaje)/100)
+// }
+// for (let index = 0; index < 5; index++) {
+//     let resultado = impuesto(parseFloat(prompt("INGRESAR PRECIO"))
+//   ,parseFloat(prompt("INGRESAR %")));
+//     alert(resultado);
+// }
+
+
+
+// Una función cotizarDolar(pesos), la cual recibe un valor monetario en pesos argentinos, y lo retorna en su equivalente en dólares.
+// Una función cotizarPesos(dolar), que recibe un valor monetario en dólares, y lo retorna en su equivalente en pesos argentinos.
+// Luego invocar las funciones solicitando  al usuario el valor y el tipo de cotización a realizar.
+
+
+// const COTIZACION_DOLAR = 150;
+// const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
+// const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
+// let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR ");
+// let valor = prompt("VALOR");
+// switch (seleccion) {
+//     case "1":
+//         alert(cotizarPesos(valor));
+//         break;
+//     case "2":
+//         alert(cotizarDolar(valor));
+//         break;
+//     default:
+//         break;
+// }
+// Codificar una función con la siguiente cabecera: validacion(cadena). 
+// En ella, se retorna un valor booleano, el cual es true si el parámetro
+//  no es un cadena vacía. Caso contrario, se retorna false. Luego invocar 
+//  la función de forma iterativa, hasta que el usuario tipee “ESC”, solicitando
+//   en cada ciclo una cadena a validar.
+
+// function validacion(cadena){
+//     return cadena != '';
+// }
+
+// let entrada =  prompt("INGRESAR CADENA");
+
+// while (entrada != 'ESC') {
+//     alert(validacion(entrada));
+//     entrada = prompt("INGRESAR CADENA");
+// }
+
+// 5)))
+// Declarar un clase Tienda que permita registrar:
+// Nombre de la tienda.
+// Dirección de la tienda.
+// Propietario de la tienda.
+// Rubro de la tienda.
+// Luego invocar al menos tres (3) objetos usando esta clase.
+
+// class Tienda{
+//     constructor(nombre, direccion, propietario, rubro){
+//         this.nombre = nombre;
+//         this.direccion = direccion;
+//         this.propietario = propietario;
+//         this.rubro = rubro;
+        
+//     }
+   
+// }
+
+
+// const tienda1 = new Tienda("Kwik-E-Mart", "Park 123", "Apu","Almacen");
+// const tienda2 = new Tienda("Edna's Edibles", "PEREZ 323", "Edna Krabappel","Panaderia");
+// const tienda3 = new Tienda("Springfield Mall", "Hall 231", "Mr. Burns","Shopping");
+// tienda1.estaAbierto()
+// console.log(tienda1);
+// console.log(tienda2);
+// console.log(tienda3);
+
+// Solicitar al usuario el registro de cinco (5) tiendas.
+// Emplear la clase Tienda de la actividad 1, para instanciar
+//  los objetos en función de las entradas capturadas. Generar
+//   una única salida compuesta por la información de los objetos instanciados.
+
+// let ingresados = '';
+// for (let index = 0; index < 5; index++) {
+//     let tienda =  new Tienda(prompt("NOMBRE"), 
+//                              prompt("DIRECCION"), 
+//                              prompt("PROPIETARIO"),
+//                              prompt("RUBRO"));
+
+//     ingresados+= "Tienda: "+tienda.nombre+" "+
+//              "Direccion: "+tienda.direccion+" "+
+//              "Propitario: "+tienda.propietario+" "+
+//              "rubro: "+tienda.rubro+"\n";
+// }
+// alert(ingresados);
+
+// Declarar un método para la clase Tienda con la siguiente
+//  cabecera estaAbierto(hora). Este retorna true si la hora 
+//  enviada está entre las 08 y 12, o entre las 15 y 19. 
+//  Caso contrario, se retorna false.
+// Luego invocar al menos un (1) objeto usando esta clase, 
+// y solicitar al usuario tres (3) horas. Informar por 
+// alerta si la tienda está abierta, en función de la hora ingresada
+
+// class Tienda{
+//     constructor(nombre, direccion, propietario, rubro){
+//         this.nombre = nombre;
+//         this.direccion = direccion;
+//         this.propietario = propietario;
+//         this.rubro = rubro;
+//     }
+//     estaAbierto(hora){
+//         if (((hora  >= 8) && (hora  <= 12))||((hora  >= 15) && (hora  <= 19))) {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+// const tienda4 = new Tienda("33 cents Store", "Cheap 231", "Barato SRL","Ropa");
+// for (let index = 0; index < 3 ; index++) {
+//     let entrada = parseInt(prompt("INGRESAR HORA EN PUNTO"));
+//     if(tienda4.estaAbierto(entrada)){
+//         alert("LA TIENDA ESTA ABIERTA A LAS "+entrada)
+//     }else{
+//         alert("LA TIENDA ESTA CERRADA A LAS "+entrada)
+//     }
+// }
+
+// Declarar un método para la clase Tienda con la siguiente cabecera 
+// esPropietario(nombre). Se retorna true si el nombre enviado 
+// corresponde al propietario de la tienda.Caso contrario, se retorna false.
+// Luego, invocar al menos tres(3) objetos usando esta clase 
+// y solicitar al usuario cinco(5) nombres. Informar por alerta
+//  si los nombres pertenecen a algún dueño de tienda.
+
+// class Tienda{
+//     constructor(nombre, direccion, propietario, rubro){
+//         this.nombre = nombre;
+//         this.direccion = direccion;
+//         this.propietario = propietario;
+//         this.rubro = rubro;
+//     }
+//       esPropietario(nombre){
+//         return this.propietario == nombre;
+//     }
+// }
+// const tienda1 = new Tienda("Kwik-E-Mart", "Park 123", "Apu", "Almacen");
+// const tienda2 = new Tienda("Edna's Edibles", "PEREZ 323", "Edna Krabappel", "Panaderia");
+// const tienda3 = new Tienda("Springfield Mall", "Hall 231", "Mr. Burns", "Shopping");
+// for (let index = 0; index < 5; index++) {
+//     let entrada = prompt("INGRESAR NOMBRE DE PROPITARIO");
+//     if (tienda1.esPropietario(entrada)){
+//         alert(entrada + " ES PROPIETARIO DE LA TIENDA " + tienda1.nombre);
+//     }
+//     if (tienda2.esPropietario(entrada)){
+//         alert(entrada + " ES PROPIETARIO DE LA TIENDA " + tienda2.nombre);
+//     }
+//     if (tienda3.esPropietario(entrada)){
+//         alert(entrada + " ES PROPIETARIO DE LA TIENDA " + tienda3.nombre);
+//     }
+// }
+
+// Declarar una clase Cliente que permita:
+// Registrar nombre, presupuesto, si tiene tarjeta de descuento, y número de teléfono del cliente.
+// Un método transferirDinero(valor), que permita  obtener y restar cierta cantidad del presupuesto,
+//  siempre que este parámetro sea menor al valor enviado, y mayor que cero.
+// Luego invocar al menos tres (3) objetos usando esta clase, y solicitar al usuario cinco
+//  (5) números. Informar por alerta si cada uno de los clientes cuenta con el presupuesto 
+//  suficiente para realizar una transferencia de igual monto al ingresado.
+
+// class Cliente{
+//     constructor(nombre,presupuesto,tarjeta,telefono){
+//         this.nombre = nombre;
+//         this.presupuesto = parseFloat(presupuesto);
+//         this.tarjeta = tarjeta;
+//         this.telefono = telefono;
+//     }
+
+//     transferirDinero(valor){
+//         if((this.presupuesto > 0)&&(valor < this.presupuesto)){
+//             this.presupuesto -= valor;
+//             return valor
+//         }else{
+//             return 0;
+//         }
+//     }
+// }
+// const cliente1 = new Cliente("Homero", 2000, true, "1234560");
+// const cliente2 = new Cliente("Carlos", "1000", false, "21234560");
+// const cliente3 = new Cliente("Barny", "50", false, "231234560");
+// for (let index = 0; index < 5; index++) {
+//     let entrada = parseFloat(prompt("INGRESAR MONTO"));
+//     if (cliente1.transferirDinero(entrada)){
+//         alert("EL CLIENTE " + cliente1.nombre+ " TE PUEDE PAGAR "+entrada);
+//     }
+//     if (cliente2.transferirDinero(entrada)){
+//         alert("EL CLIENTE " + cliente2.nombre+ " TE PUEDE PAGAR "+entrada);
+//     }
+//     if (cliente3.transferirDinero(entrada)){
+//         alert("EL CLIENTE " + cliente3.nombre+ " TE PUEDE PAGAR "+entrada);
+//     }else{alert("nadie puede")}
+// }
---
+++ clase5.js
@@ -0,0 +1,29 @@
+
+// const persona1={nombre:"juan",edad:23, calle:"frencj"}
+
+// // console.log("nombre" in persona1)
+
+// // for (const carateristicas in persona1){ console.log(persona1[carateristicas])}
+
+// class Producto{
+//     constructor(nombre,precio){
+//         this.nombre=nombre;
+//         this.precio=precio
+//         this.vendido=false;
+//     }
+//     sumarIva(){
+//         this.precio=this.precio*1,21
+//     }
+//     vender(){this.vendido=true}
+// }
+
+// const producto1= new Producto("homer",34)
+// const producto2=new Producto("tuvi",23)
+
+// producto1.sumarIva()
+// producto2.sumarIva()
+// producto1.vender() //vendido
+
+// console.log(producto1)
+// console.log(producto2)
+
